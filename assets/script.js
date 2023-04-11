3e346d15dc4e184622685c7874775711aa35c322
Douglas Lima <douglas07a@gmail.com>
2023-04-11 12:25:10 -0300

    Função PDF

---
+++ assets/script.js
@@ -0,0 +1,28 @@
+function gerarPdf() {
+    let itens = document.querySelector('body').innerHTML
+
+    let conteudo = window.open('','','width=2480px,heigth=3508px')
+    conteudo.document.write('<html><head>')
+    conteudo.document.write('<title>Ficha de inscrição</title></head>')
+    conteudo.document.write('<body')
+    conteudo.document.write(itens)
+    conteudo.document.write('</body></html>')
+    conteudo.document.close()
+    conteudo.print()
+  
+        
+}
+
+
+ /*
+    const item = document.querySelector('body').innerHTML
+
+    
+    var opt = {
+        margin: 1,
+        filename: 'ficha.pdf',
+        html2canvas: {scala: 1},
+        jsPDF: {unit: 'in', format: 'letter', orientation: 'portrait'},
+    }
+
+        html2pdf().set(opt).from(item).save();*/
